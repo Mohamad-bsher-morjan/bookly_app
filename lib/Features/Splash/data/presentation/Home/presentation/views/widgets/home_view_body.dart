60099f1fd8e8949c806a3b2dd8404504b83e4ed1
bsher morjan <bshermorgan4@gmail.com>
2025-12-05 21:37:51 +0300

    create app styles

--- lib/Features/Splash/data/presentation/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Splash/data/presentation/Home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,10 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           const SizedBox(height: 50),
-          Text('Best Seller ', style: Styles.titleMedium),
+          Text(
+            'Best Seller ',
+            style: Styles.textStyle18.copyWith(fontFamily: kGTSectraFine),
+          ),
           BestSellerListVierItem(),
         ],
       ),
@@ -30,21 +34,23 @@ class BestSellerListVierItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(height: 125,
+    return SizedBox(
+      height: 125,
       child: Row(
         children: [
-         AspectRatio(aspectRatio: 2.5/4,
-          child: Container(
-            decoration:  BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.amber,
-              image:const DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(AssetsData.testImage),
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.amber,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
               ),
             ),
           ),
-        ),
           Column(children: []),
         ],
       ),
