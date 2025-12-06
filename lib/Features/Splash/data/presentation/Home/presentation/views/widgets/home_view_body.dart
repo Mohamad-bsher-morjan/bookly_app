b6ebb17e3bbd6cf4a8507cc97f94fab537b1a20f
bsher morjan <bshermorgan4@gmail.com>
2025-12-06 13:35:06 +0300

    best seller item part 3

--- lib/Features/Splash/data/presentation/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Splash/data/presentation/Home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
+import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/best_seller_list_veiw_item.dart';
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -23,49 +23,7 @@ class HomeViewBody extends StatelessWidget {
             style: Styles.textStyle18.copyWith(fontFamily: kGTSectraFine),
           ),
           const SizedBox(height: 20),
-          BestSellerListVierItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListVierItem extends StatelessWidget {
-  const BestSellerListVierItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.amber,
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 30),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire',
-                  style: Styles.textStyle20,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ],
-          ),
+          BestSellerListViewItem(),
         ],
       ),
     );
