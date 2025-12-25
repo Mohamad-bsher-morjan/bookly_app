bce342bec2ef3e020d152ad6277575b4a4adcca2
bsher morjan <bshermorgan4@gmail.com>
2025-12-25 16:26:55 +0300

    add custom book item to book details view

--- lib/Features/Splash/data/presentation/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Splash/data/presentation/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -8,7 +9,10 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: const Column(children: [CustomBookDetailsAppBar()]),
+      child: const Column(children: [CustomBookDetailsAppBar(),
+      
+      CustomBookImage(),
+      ]),
     );
   }
 }
