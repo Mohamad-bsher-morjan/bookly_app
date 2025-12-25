ca532b265a8537532c10549c48a317c60ca83330
bsher morjan <bshermorgan4@gmail.com>
2025-12-25 19:32:38 +0300

    add rating to book details

--- lib/Features/Splash/data/presentation/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Splash/data/presentation/Home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/Splash/data/presentation/Home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly_app/core/utils/styles.dart';
@@ -32,6 +33,8 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: 18,),
+         const BookRating(mainAxisAlignment: MainAxisAlignment.center,),
         ],
       ),
     );
