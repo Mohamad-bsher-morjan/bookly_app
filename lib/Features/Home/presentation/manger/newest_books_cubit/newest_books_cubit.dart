1389751531e6cb075fd889ecbfc0130be93d5cd0
bsher morjan <bshermorgan4@gmail.com>
2026-01-03 20:46:26 +0300

    newest books cubit done

---
+++ lib/Features/Home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewestBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(errMessage: failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books: books));
+      },
+    );
+  }
+
+}
