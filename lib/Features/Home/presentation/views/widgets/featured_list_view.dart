4f64ef8200d0a5f59f14a7767c28f2f449b1b959
bsher morjan <bshermorgan4@gmail.com>
2026-01-12 14:37:16 +0300

    disply featured books  part 1

--- lib/Features/Home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/Home/presentation/views/widgets/featured_list_view.dart
@@ -1,22 +1,39 @@
+import 'package:bookly_app/Features/Home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/custom_book_item.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            child: CustomBookImage(),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: CustomBookImage(),
+                );
+              },
+            ),
           );
-        },
-      ),
+        
+        }
+        else if (state is FeaturedBooksFailure){
+          return CustomErrorWidget(errMessage: state.errMesaage);
+        }
+        else{
+          return CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
